0c5c8702b9cba7e2655dade11839eb2bf8d40c65
Randall Mason <Randall@Mason.CH>
2021-12-22 21:09:14 -0600

    Play with gopls

--- day17/day17.go
+++ day17/day17.go
@@ -9,35 +9,35 @@ import (
 	"strings"
 )
 
-func y(n int64, dy0 int64) int64 {
+func calculateY(n int64, dy0 int64) int64 {
 	if n == 0 {
 		return 0
 	}
-	answer := y(n-1, dy0) + dy(n-1, dy0)
+	answer := calculateY(n-1, dy0) + calculateDY(n-1, dy0)
 	return answer
 }
 
-func dy(n int64, dy0 int64) int64 {
+func calculateDY(n int64, dy0 int64) int64 {
 	if n == 0 {
 		return dy0
 	}
-	dyminus1 := dy(n-1, dy0)
+	dyminus1 := calculateDY(n-1, dy0)
 	return dyminus1 - 1
 }
 
-func x(n int64, dx0 int64) int64 {
+func calculateX(n int64, dx0 int64) int64 {
 	if n == 0 {
 		return 0
 	}
-	answer := x(n-1, dx0) + dx(n-1, dx0)
+	answer := calculateX(n-1, dx0) + calculateDX(n-1, dx0)
 	return answer
 }
 
-func dx(n int64, dx0 int64) int64 {
+func calculateDX(n int64, dx0 int64) int64 {
 	if n == 0 {
 		return dx0
 	}
-	dxminus1 := dx(n-1, dx0)
+	dxminus1 := calculateDX(n-1, dx0)
 	if dxminus1 > 0 {
 		return dxminus1 - 1
 	} else if dxminus1 < 0 {
@@ -61,12 +61,12 @@ func main() {
 	r, _ := regexp.Compile("([a-z]+)=(-?[0-9]+)..(-?[0-9]+)")
 
 	x_range := r.FindAllStringSubmatch(string(data), -1)[0]
-	x_min, err := strconv.ParseInt(x_range[2], 0, 64)
-	x_max, err := strconv.ParseInt(x_range[3], 0, 64)
+	x_min, _ := strconv.ParseInt(x_range[2], 0, 64)
+	x_max, _ := strconv.ParseInt(x_range[3], 0, 64)
 
 	y_range := r.FindAllStringSubmatch(string(data), -1)[1]
-	y_min, err := strconv.ParseInt(y_range[3], 0, 64)
-	y_max, err := strconv.ParseInt(y_range[2], 0, 64)
+	y_min, _ := strconv.ParseInt(y_range[3], 0, 64)
+	y_max, _ := strconv.ParseInt(y_range[2], 0, 64)
 
 	x_vel := int64(x_max)
 	x_prev := int64(0)
@@ -76,16 +76,16 @@ func main() {
 	for x_vel > 0 {
 		changed := true
 		n_term := int64(0)
-		for n := int64(1); x(n, x_vel) <= x_max && changed; n++ {
-			if x_prev == x(n, x_vel) {
+		for n := int64(1); calculateX(n, x_vel) <= x_max && changed; n++ {
+			if x_prev == calculateX(n, x_vel) {
 				changed = false
 			} else {
-				x_prev = x(n, x_vel)
+				x_prev = calculateX(n, x_vel)
 			}
 			n_term = n
-			x_term := x(n_term, x_vel)
+			x_term := calculateX(n_term, x_vel)
 			if x_term >= x_min && x_term <= x_max {
-				if changed == false {
+				if !changed {
 					n_list_for_x_infinate[n_term] = append(n_list_for_x_infinate[n_term], []int64{x_term, x_vel})
 					// fmt.Println(n_term, x_vel, x(n_term, x_vel), "no change")
 				} else {
@@ -102,9 +102,9 @@ func main() {
 
 	for y_vel > int64(y_max)-1 {
 		n_term := int64(0)
-		for n := int64(1); y(n, y_vel) >= y_max; n++ {
+		for n := int64(1); calculateY(n, y_vel) >= y_max; n++ {
 			n_term = n
-			y_term := y(n_term, y_vel)
+			y_term := calculateY(n_term, y_vel)
 			if y_term <= y_min && y_term >= y_max {
 				n_list_for_y[n_term] = append(n_list_for_y[n_term], []int64{y_term, y_vel})
 			}
@@ -127,15 +127,15 @@ func main() {
 		}
 	}
 	max := int64(0)
-	for speed, _ := range valid_speeds {
+	for speed := range valid_speeds {
 		if speed[1] > max {
 			max = speed[1]
 		}
 
 	}
 	last_y := int64(0)
-	for n := int64(1); y(n, max) > last_y; n++ {
-		last_y = y(n, max)
+	for n := int64(1); calculateY(n, max) > last_y; n++ {
+		last_y = calculateY(n, max)
 	}
 	fmt.Println(last_y, len(valid_speeds))
 	// n := int64(0)
---
+++ day17/go.mod
@@ -0,0 +1,3 @@
+module haun.ch/aoc2021/day17
+
+go 1.17
